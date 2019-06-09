d09257d1cc367cb5200f7343bd20f27aad5de787
keeonline <keeonline@outlook.com>
2019-06-09 06:37:38 +0100

    set java version to 1.8

--- src/main/java/keeonline/LibraryController.java
+++ src/main/java/keeonline/LibraryController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
  * "C:\Program Files\Java\jdk-11.0.1\bin\java" -jar library-0.0.1-SNAPSHOT.jar
  * 
  * curl  http://localhost:8080/library/v1/ping
- * curl -X PUT -H "Content-Type: application/json" -d "{\"title\":\"John With The Wind\",\"publicationId\": 11111,\"category\": \"Fiction\"}" http://localhost:8080/library/v1/books?copies=3
- * curl -X PUT -H "Content-Type: application/json" -d "{\"title\":\"The Rise and Fall of the Gnomen Empire\",\"publicationId\": 22222,\"category\": \"Fiction\"}" http://localhost:8080/library/v1/books?copies=3
- * curl -X PUT -H "Content-Type: application/json" -d "{\"title\":\"A Tale Of Two Mittens\",\"publicationId\": 33333,\"category\": \"Fiction\"}" http://localhost:8080/library/v1/books?copies=3
- * curl -X PUT -H "Content-Type: application/json" -d "{\"title\":\"Waterloo\",\"publicationId\": 44444,\"category\": \"History\"}" http://localhost:8080/library/v1/books?copies=3
- * curl -X PUT -H "Content-Type: application/json" -d "{\"title\":\"3:10 to Ongar\",\"genre\": \"Western\"}}" http://localhost:8080/library/v1/films?copies=2
- * curl -X PUT -H "Content-Type: application/json" -d "{\"title\":\"The Shirt Locker\",\"genre\": \"Action\"}}" http://localhost:8080/library/v1/films?copies=4
- * curl -X PUT -H "Content-Type: application/json" -d "{\"firstName\":\"Joe\",\"lastName\":\"Bloggs\",\"dob\": \"2011-12-03\",\"houseNumber\": 22,\"postCode\": \"AB1CD23\"}}" http://localhost:8080/library/v1/customers
+ * curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"John With The Wind\",\"publicationId\": 11111,\"category\": \"Fiction\"}" http://localhost:8080/library/v1/books?copies=3
+ * curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"The Rise and Fall of the Gnomen Empire\",\"publicationId\": 22222,\"category\": \"Fiction\"}" http://localhost:8080/library/v1/books?copies=3
+ * curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"A Tale Of Two Mittens\",\"publicationId\": 33333,\"category\": \"Fiction\"}" http://localhost:8080/library/v1/books?copies=3
+ * curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"Waterloo\",\"publicationId\": 44444,\"category\": \"History\"}" http://localhost:8080/library/v1/books?copies=3
+ * curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"3:10 to Ongar\",\"genre\": \"Western\"}}" http://localhost:8080/library/v1/films?copies=2
+ * curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"The Shirt Locker\",\"genre\": \"Action\"}}" http://localhost:8080/library/v1/films?copies=4
+ * curl -X POST -H "Content-Type: application/json" -d "{\"firstName\":\"Joe\",\"lastName\":\"Bloggs\",\"dob\": \"2011-12-03\",\"houseNumber\": 22,\"postCode\": \"AB1CD23\"}}" http://localhost:8080/library/v1/customers
  * curl http://localhost:8080/library/v1/books
  * curl http://localhost:8080/library/v1/films
  * curl http://localhost:8080/library/v1/products
